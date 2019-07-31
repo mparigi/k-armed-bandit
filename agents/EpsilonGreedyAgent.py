3a4e9cd9aea309fca673fbd6843f2f2cfbffd35d
Makarand Parigi <mparigi@twitter.com>
2019-07-30 17:19:01 -0700

    implemented more efficient update rule for Q table

--- agents/EpsilonGreedyAgent.py
+++ agents/EpsilonGreedyAgent.py
@@ -6,8 +6,8 @@ class EpsilonGreedyAgent():
 		assert k > 0
 		self.k = k
 		self.epsilon = epsilon
-		# (total reward gained from this action, times this action was selected)
-		self.sample_average = [(0, 0)] * k
+		# (Q(i), num times selected)
+		self.q_values = [(0, 0)] * k
 
 	def action(self):
 		# decide if we explore or exploit
@@ -18,12 +18,15 @@ class EpsilonGreedyAgent():
 			action = random.randint(0, self.k - 1)
 		else: # exploit
 			# find the action(s) with the highest Q value
-			q_values = [0 if n == 0 else r / n for r, n in self.sample_average]
-			max_q = max(q_values)
-			best_action_indices = [i for i, j in enumerate(q_values) if j == max_q]
+			max_q = max(self.q_values)
+			best_action_indices = [i for i, j in enumerate(self.q_values) if j == max_q]
 			action = random.choice(best_action_indices) # break ties randomly
 		return action
 
 
 	def update(self, action, reward):
-		self.sample_average[action] = (self.sample_average[action][0] + reward, self.sample_average[action][1] + 1)
+		old_q, old_n = self.q_values[action]
+		self.q_values[action] = (
+			old_q + (1/(old_n + 1)) * (reward - old_q), # Q update rule
+			old_n + 1
+			)
